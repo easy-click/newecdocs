5174c375967873ed6f4bb10e7bd6c7541fb320b4
XINLAN <kyowxf2003@163.com>
2022-09-09 15:09:15 +0800

    3

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-#nvm use 16.16
-yarn build
-cp -r build/* ../ecdocs/
+DEST='/Users/wangbx/git_wysource/fd/ec-cloud/easyclick-devtools/src/main/resources/newdocs'
+SRC='/Users/wangbx/GitHub/newecdocs'
+cd $SRC
+echo `pwd`
+export PATH=/Users/wangbx/.nvm/versions/node/v16.16.0/bin/:$PATH
+/Users/wangbx/.nvm/versions/node/v16.16.0/bin/npm run build
+
+cd $DEST
+echo `pwd`
+rm -r *
+cp -r $SRC/build/* ./
+rm -r en/
