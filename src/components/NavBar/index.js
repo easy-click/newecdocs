a35b8fa94d9da033fe3a6c8aa48c88472dce2d27
XINLAN <kyowxf2003@163.com>
2022-12-19 13:53:58 +0800

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -40,24 +40,7 @@ export default function Index(props) {
           {/* 右侧列表 */}
           <div className={styles.nav_container}>
             <ul className={styles.nav_lists}>
-              <li>
-                <div className="dropdown dropdown--hoverable">
-                  云控/群控产品
-                  <img className={styles.iconDropDown}
-                       src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAABUUlEQVRoge3Wr0tdcRzH4WfeKcpwCCIMBAXBIlgWFpYMppUVLUs2i9F/4SbXtrBmsmhZWVpYWrhhRVgZCBMGggiiyIZjTsO9XyZ3XO65P86P8HngE88571c7hBBCCCGEEEIYujpuK371rDE7FRjb6d5kjYAHeFeB0e23i5FeQrQe2KvA+HQHqPUakdSwX4GI9xjtNyIZw4cSIz5ifNCIZAKfSoj4jEfDikgeo1FgRAOTw45IpvClgIhDTOcVkczga44R3/Ak74hkFkc5RBxjvqiIZA7fBxx+/35godCCexZx0mVgljvFUsHb/7OMM/1HnONp4as7eIZLvUdctJ6tlOe4kj3iJ1ZKWZrBKn7pHnGNFyVtzOwlfusc8Qfrpa3r0Zrm4PaIG7wqcVdfNjSHp4i/2Cx10QC2/AvZzvNDD/N8Od5q/jXD65y/FUIIIYRQNXfPaMyAru8lkAAAAABJRU5ErkJggg=="/>
-                  <ul className="dropdown__menu">
-                    <li>
-                      <a className={`${styles.DropDownFont} dropdown__link`} href="/enterprise_server">
-                       企业版云控</a>
-                    </li>
-                    <li>
-                      <a className={`${styles.DropDownFont} dropdown__link`} href="/local_ctrl_server">
-                        企业版群控</a>
-                    </li>
 
-                  </ul>
-                </div>
-              </li>
               <li>
                 <div className="dropdown dropdown--hoverable">
                   自动化产品
@@ -78,7 +61,7 @@ export default function Index(props) {
               </li>
               <li>
                 <div className="dropdown dropdown--hoverable">
-                  开发文档
+                  技术开发文档
                   <img className={styles.iconDropDown}
                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAABUUlEQVRoge3Wr0tdcRzH4WfeKcpwCCIMBAXBIlgWFpYMppUVLUs2i9F/4SbXtrBmsmhZWVpYWrhhRVgZCBMGggiiyIZjTsO9XyZ3XO65P86P8HngE88571c7hBBCCCGEEEIYujpuK371rDE7FRjb6d5kjYAHeFeB0e23i5FeQrQe2KvA+HQHqPUakdSwX4GI9xjtNyIZw4cSIz5ifNCIZAKfSoj4jEfDikgeo1FgRAOTw45IpvClgIhDTOcVkczga44R3/Ak74hkFkc5RBxjvqiIZA7fBxx+/35godCCexZx0mVgljvFUsHb/7OMM/1HnONp4as7eIZLvUdctJ6tlOe4kj3iJ1ZKWZrBKn7pHnGNFyVtzOwlfusc8Qfrpa3r0Zrm4PaIG7wqcVdfNjSHp4i/2Cx10QC2/AvZzvNDD/N8Od5q/jXD65y/FUIIIYRQNXfPaMyAru8lkAAAAABJRU5ErkJggg=="/>
                   <ul className="dropdown__menu">
@@ -91,6 +74,24 @@ export default function Index(props) {
                     <li>
                       <a className={`${styles.DropDownFont} dropdown__link`} href="/iostjdocs">iOS 脱机版开发文档</a>
                     </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <div className="dropdown dropdown--hoverable">
+                  云控/群控产品
+                  <img className={styles.iconDropDown}
+                       src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAABUUlEQVRoge3Wr0tdcRzH4WfeKcpwCCIMBAXBIlgWFpYMppUVLUs2i9F/4SbXtrBmsmhZWVpYWrhhRVgZCBMGggiiyIZjTsO9XyZ3XO65P86P8HngE88571c7hBBCCCGEEEIYujpuK371rDE7FRjb6d5kjYAHeFeB0e23i5FeQrQe2KvA+HQHqPUakdSwX4GI9xjtNyIZw4cSIz5ifNCIZAKfSoj4jEfDikgeo1FgRAOTw45IpvClgIhDTOcVkczga44R3/Ak74hkFkc5RBxjvqiIZA7fBxx+/35godCCexZx0mVgljvFUsHb/7OMM/1HnONp4as7eIZLvUdctJ6tlOe4kj3iJ1ZKWZrBKn7pHnGNFyVtzOwlfusc8Qfrpa3r0Zrm4PaIG7wqcVdfNjSHp4i/2Cx10QC2/AvZzvNDD/N8Od5q/jXD65y/FUIIIYRQNXfPaMyAru8lkAAAAABJRU5ErkJggg=="/>
+                  <ul className="dropdown__menu">
+                    <li>
+                      <a className={`${styles.DropDownFont} dropdown__link`} href="/enterprise_server">
+                        企业版云控</a>
+                    </li>
+                    <li>
+                      <a className={`${styles.DropDownFont} dropdown__link`} href="/local_ctrl_server">
+                        企业版群控</a>
+                    </li>
+
                   </ul>
                 </div>
               </li>
